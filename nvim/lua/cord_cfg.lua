4a7a0f19ff5c4069b60fed6d0938c5e346c8efef
GalenYost <brawlstarser086@gmail.com>
2025-07-17 14:22:21 +0300

    cord changes

--- nvim/lua/cord_cfg.lua
+++ nvim/lua/cord_cfg.lua
@@ -3,16 +3,9 @@ require('cord').setup {
       tooltip = 'God-Tier Text Editor',
    },
    idle = {
-      enabled = true,
-      timeout = 450000,
+      enabled = false,
    },
    text = {
       workspace = '',
-   },
-   filter = function()
-      if vim.bo.filetype == "snacks_picker_list" then
-         return false
-      end
-      return true
-   end,
+   }
 }
