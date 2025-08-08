6a9dbd7d95897a362d3b8d65f90c181fe7976a39
GalenYost <brawlstarser086@gmail.com>
2025-08-08 15:24:20 +0300

    returned tab keybinds in normal and visual modes

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -3,6 +3,11 @@ local keymap = vim.keymap
 
 keymap.set("n", "<Esc>", ":nohlsearch<CR>", opts)
 
+keymap.set("n", "<Tab>", ">>", opts)
+keymap.set("n", "<S-Tab>", "<<", opts)
+keymap.set("v", "<Tab>", ">gv", opts)
+keymap.set("v", "<S-Tab>", "<gv", opts)
+
 for _, mode in ipairs({ "n", "v", "i" }) do
    vim.api.nvim_set_keymap(mode, '<Up>', '<Nop>', opts)
    vim.api.nvim_set_keymap(mode, '<Down>', '<Nop>', opts)
